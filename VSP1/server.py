bf72924c7e18e0abb7a1a1fb7cb08f51760440fe
archi221 <98173934+archi221@users.noreply.github.com>
2025-11-05 13:38:16 +0100

    first commit

---
+++ VSP1/server.py
@@ -0,0 +1,69 @@
+from socket import *
+import json
+class Server:
+    def run(self):
+        s = socket(AF_INET, SOCK_STREAM)
+        s.bind(("172.18.27.132", 7777))
+        s.listen(1)
+        (conn, addr) = s.accept() # returns new socket and addr. client
+        while True: # forever
+            bytes = conn.recv(1024) # receive data from client
+
+            if not bytes: continue
+            data = bytes.decode()
+            data_unmarsh = json.loads(data)
+
+            if data_unmarsh["method"] == "write":
+                try:
+                    write(data_unmarsh["v1"], data_unmarsh["v2"])
+                except Exception as e:
+                    answer = {
+                        "ok": 0,
+                        "return": f"Error: {e}"
+                    }
+                else:
+                        read_answer = 0
+
+                        answer = {
+                        "ok": 1,
+                        "return": 0
+                    }
+                
+            elif data_unmarsh["method"] == "read":
+                try:
+                    read_answer = read(data_unmarsh["v1"])
+                except IndexError:
+                    answer = {
+                        "ok": 0,
+                        "return": "NoSuchElementException Index wurde nicht gefunden"
+                    }
+                except Exception as e:
+                    answer = {
+                        "ok": 0,
+                        "return": f"Error: {e}"
+                    }
+                else:
+                    answer = {
+                    "ok": 1,
+                    "return": read_answer
+                }
+            else : break
+            print(data_unmarsh)
+            print(daten)
+            
+
+            json_data = json.dumps(answer)
+            bytes_data = json_data.encode()
+            conn.send(bytes_data)
+
+        conn.close() # close the connection
+
+daten = ["cat", "dog", "papagei", "pigeon", "hamster", "lion", "dino", "koala"]
+def write(i: int, d:str):
+    daten[i] = d
+
+def read(i: int): 
+    return daten[i]
+
+s = Server
+s.run(s)
