3033decc0350e193f4c3786cfbaf94ea2d11480a
Leif-ErikF <leif-erik.fischer@haw-hamburg.de>
2025-11-19 16:18:50 +0100

    Updated: still broken

--- VSP2/receiver.py
+++ VSP2/receiver.py
@@ -16,7 +16,7 @@ def main() -> None:
         print(f"Error: {e}")
         exit()
     else:
-        bytes = s.recv(1024) # receive the response
+        bytes = s.recv(4096) # receive the response
         json_data = bytes.decode()
         data = json.loads(json_data)
         amount = data["amount"]
@@ -24,22 +24,31 @@ def main() -> None:
         nextNeighbor = data["neighbor2"]
         list_numbers = data["list_numbers"]
 
-        # Eigenen Server starten
-        HOST, PORT = "127.0.0.1", 7777
-        asyncio.create_task(start_server(HOST, PORT))
-
-        # Mit nächstem Nachbarn verbinden
-        HOST_NEIGHBOR, PORT_NEIGHBOR = nextNeighbor
-        connection_neighbor = asyncio.create_task(connectToNeighbor(HOST_NEIGHBOR, PORT_NEIGHBOR))
-
-        task_listenToServer = asyncio.create_task(listenToServer(task_listenToServer))
-
+        # Variable Anzahl an Threads
+        anzahl_threads = data["amount"]
+        # Mein Port, für die Threads dann + i
+        port = 7777
+        # Anzahl Threads starten
+        with concurrent.futures.ThreadPoolExecutor(max_workers=anzahl_threads) as executor:  # Einzelne Threads starten, keinen Pool
+            for i in range(anzahl_threads):
+                executor.submit(asyncio.run(async_main(prevNeighbor, nextNeighbor, list_numbers[i]), port+i), i)
 
 async def start_server(host, port):
-    server = await asyncio.start_server(handle_prev_neighbor, host, port)
+    server = await asyncio.start_server(handle_neighbor, host, port)
     print(f"Lausche auf {port}")
 
+async def connectToPrevNeighbor(host, port):
+    # Verbinde dich zum nächsten Nachbarn
+    prev_reader, prev_writer = await asyncio.open_connection(host, port)
+    print("Verbunden mit nächstem Nachbarn")  
+
+async def connectToNextNeighbor(host, port):
+    # Verbinde dich zum nächsten Nachbarn
+    next_reader, next_writer = await asyncio.open_connection(host, port)
+    print("Verbunden mit nächstem Nachbarn")
+
 async def listenToServer(reader_server):
+    # Auf Nachrichten vom Server warten
     while True:
             data = await reader_server.readline()
             if not data:
@@ -47,46 +56,32 @@ async def listenToServer(reader_server):
                 break
             SERVER_VALUE = int(data.decode().strip())
 
-async def connectToNeighbor(self, host, port):
-    # Verbinde dich zum nächsten Nachbarn
-    self.next_reader, self.next_writer = await asyncio.open_connection(host, port)
-    print("Verbunden mit nächstem Nachbarn")
-
-
-
-
-
-
-
-
-
-
-
-
-
-async def listen(self):
-    bytes = self.s.recv(1024) # receive the response
-    json_data = bytes.decode()
-    data = json.loads(json_data)
-
-    # Variable Anzahl an Threads
-    anzahl_threads = data["anzahl"]
-    # Anzahl Threads starten
-    with concurrent.futures.ThreadPoolExecutor(max_workers=anzahl_threads) as executor:
-        for i in range(anzahl_threads):
-            executor.submit(ggT, i)
-
-
-def ggT():
-    return
-
-async def handle_prev_neighbor(self, reader, writer):
-        # Nimm Daten vom vorherigen Nachbarn entgegen
-        while True:
-            data = await reader.readline()
+async def listenToPrevNeighbor():
+    # Auf Nachrichten vom vorherigen Nachbarn warten
+    while True:
+            data = await reader_server.readline()
             if not data:
+                print("Verbindung vom Server getrennt")
                 break
-            PREV_VALUE = data
-            print("Vom vorherigen Nachbarn:", data.decode().strip())
+            SERVER_VALUE = int(data.decode().strip())
 
+async def async_main(prevNeighbor, nextNeighbor, number, port):
+    M = number
+    # Eigenen Server starten
+    HOST, PORT = "127.0.0.1", port
+    server = asyncio.create_task(start_server(HOST, PORT))
+    # Mit nächstem Nachbarn verbinden
+    HOST_PREV_NEIGHBOR, PORT_PREV_NEIGHBOR = prevNeighbor
+    HOST_NEXT_NEIGHBOR, PORT_NEXT_NEIGHBOR = nextNeighbor
+
+    connectionToPrevNeighbor = asyncio.create_task(connectToPrevNeighbor(HOST_PREV_NEIGHBOR, PORT_PREV_NEIGHBOR))
+    connectionToNextNeighbor = asyncio.create_task(connectToNextNeighbor(HOST_NEXT_NEIGHBOR, PORT_NEXT_NEIGHBOR))
+
+    task_listenToServer = asyncio.create_task(listenToServer(task_listenToServer))
+    task_listenToPrevNeighbor = asyncio.create_task(listenToPrevNeighbor)
+    task_listenToNext_Neigbor = asyncio.create_task(listenToNext_Neigbor)
+    task_ggT = asyncio.create_task(ggT)
+
+async def handle_neighbor():
+    return
 
